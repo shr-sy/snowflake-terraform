7f9ef0635c08b3477760e2d09e1b320e136e2957
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 15:20:11 +0530

    Update qa_role.tf

--- roles/qa_role.tf
+++ roles/qa_role.tf
@@ -1,8 +1,3 @@
 resource "snowflake_role" "qa_role" {
   name = "QA_ROLE"
 }
-
-resource "snowflake_role_grants" "qa_grant_user" {
-  role_name = snowflake_role.qa_role.name
-  users     = [snowflake_user.qa_user.name]
-}
