61e7b814e2e2dcfbad0a2b8a214c2c8b120b2244
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 16:45:03 +0530

    Update dev_user.tf

--- users/dev_user.tf
+++ users/dev_user.tf
@@ -1,7 +1,6 @@
-
 resource "snowflake_user" "dev_user" {
-  name         = "DEV_USER"
-  password     = "DevPass123!"
-  default_role = "DEV_ROLE"
-  disabled     = false
+  name     = "DEV_USER"
+  password = "DevUser@123"
+  comment  = "Developer user"
+  disabled = false
 }
