53b0bd0b02e8bbf1cbc5730e30dd571c1955b2e8
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 15:40:36 +0530

    Update prod_user.tf

--- users/prod_user.tf
+++ users/prod_user.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "snowflake-labs/snowflake"
+      version = ">= 1.0.0"
+    }
+  }
+}
+
 resource "snowflake_user" "prod_user" {
   name         = "PROD_USER"
   password     = "ProdPass123!"
