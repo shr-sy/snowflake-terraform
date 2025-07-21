c641d217184557a7d2781aa4feaac08ea0380d09
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 15:42:44 +0530

    Update dev_role.tf

--- roles/dev_role.tf
+++ roles/dev_role.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "snowflake-labs/snowflake"
+      version = ">= 1.0.0"
+    }
+  }
+}
+
 resource "snowflake_role" "dev_role" {
   name = "DEV_ROLE"
 }
