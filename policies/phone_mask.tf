e96b1090d26678876b2882c15a1fc01203945eae
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 15:23:49 +0530

    Update phone_mask.tf

--- policies/phone_mask.tf
+++ policies/phone_mask.tf
@@ -1,8 +1,19 @@
+
 resource "snowflake_masking_policy" "phone_mask" {
-  name               = "PHONE_MASK"
-  database           = "MY_DB"
-  schema             = "PUBLIC"
-  expression         = "CASE WHEN CURRENT_ROLE() = 'PROD_ROLE' THEN val ELSE 'MASKED' END"
-  return_data_type   = "STRING"
-  value_data_type    = "STRING"
+  name     = "PHONE_MASK"
+  database = "YOUR_DB"
+  schema   = "YOUR_SCHEMA"
+
+  signature {
+    column_names = ["phone"]
+    column_types = ["VARCHAR"]
+  }
+
+  return_data_type = "VARCHAR"
+  body = <<-SQL
+    CASE
+      WHEN CURRENT_ROLE() IN ('ACCOUNTADMIN', 'FULL_ACCESS') THEN phone
+      ELSE 'XXXXXXXXXX'
+    END
+  SQL
 }
