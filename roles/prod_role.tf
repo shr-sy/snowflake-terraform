c2fda96aa529816d5f971099e7d30592d7359a78
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 15:51:58 +0530

    Update prod_role.tf

--- roles/prod_role.tf
+++ roles/prod_role.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "snowflake-labs/snowflake"
-      version = ">= 1.0.0"
-    }
-  }
-}
 
 resource "snowflake_role" "prod_role" {
   name = "PROD_ROLE"
