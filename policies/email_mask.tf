d1bdc7d37e37f19e726eb6af43d2f94faaa089f4
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 15:22:37 +0530

    Update email_mask.tf

--- policies/email_mask.tf
+++ policies/email_mask.tf
@@ -1,8 +1,19 @@
+
 resource "snowflake_masking_policy" "email_mask" {
-  name               = "EMAIL_MASK"
-  database           = "MY_DB"
-  schema             = "PUBLIC"
-  expression         = "CASE WHEN CURRENT_ROLE() IN ('DEV_ROLE') THEN val ELSE 'MASKED' END"
-  return_data_type   = "STRING"
-  value_data_type    = "STRING"
+  name     = "EMAIL_MASK"
+  database = "YOUR_DB"
+  schema   = "YOUR_SCHEMA"
+
+  signature {
+    column_names = ["email"]
+    column_types = ["VARCHAR"]
+  }
+
+  return_data_type = "VARCHAR"
+  body = <<-SQL
+    CASE
+      WHEN CURRENT_ROLE() IN ('ACCOUNTADMIN', 'FULL_ACCESS') THEN email
+      ELSE '***MASKED***'
+    END
+  SQL
 }
