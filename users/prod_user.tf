ea2be297b95917858b83e7511a06220fe851ceaf
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 16:48:53 +0530

    Update prod_user.tf

--- users/prod_user.tf
+++ users/prod_user.tf
@@ -1,6 +1,6 @@
 resource "snowflake_user" "prod_user" {
-  name         = "PROD_USER"
-  password     = "ProdPass123!"
-  default_role = "PROD_ROLE"
-  disabled     = false
+  name     = "PROD_USER"
+  password = "ProdUser@123"
+  comment  = "Production user"
+  disabled = false
 }
