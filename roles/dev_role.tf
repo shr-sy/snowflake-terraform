f2f893200b7c2eb9acb114b251a43beea1709367
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 15:18:48 +0530

    Update dev_role.tf

--- roles/dev_role.tf
+++ roles/dev_role.tf
@@ -2,7 +2,3 @@ resource "snowflake_role" "dev_role" {
   name = "DEV_ROLE"
 }
 
-resource "snowflake_role_grants" "assign_dev_user" {
-  role_name = snowflake_role.dev_role.name
-  users     = [snowflake_user.dev_user.name]
-}
