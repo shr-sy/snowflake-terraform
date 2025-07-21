96d58820b3034f26a2ef9881da9288a817315754
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 15:41:00 +0530

    Update qa_user.tf

--- users/qa_user.tf
+++ users/qa_user.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "snowflake-labs/snowflake"
+      version = ">= 1.0.0"
+    }
+  }
+}
+
 resource "snowflake_user" "qa_user" {
   name         = "QA_USER"
   password     = "QaPass123!"
