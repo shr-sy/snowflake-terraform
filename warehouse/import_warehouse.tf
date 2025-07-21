203d6957651c7f7faf7b950efa369da82b05e6e6
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 15:54:17 +0530

    Update import_warehouse.tf

--- warehouse/import_warehouse.tf
+++ warehouse/import_warehouse.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "snowflake-labs/snowflake"
-      version = ">= 1.0.0"
-    }
-  }
-}
-
 resource "snowflake_warehouse" "prod_wh" {
   name = "PROD_WH"
 }
