0f04442e4d695bc2531dd268e9ebf934d639daa4
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 15:20:50 +0530

    Update prod_role.tf

--- roles/prod_role.tf
+++ roles/prod_role.tf
@@ -2,7 +2,3 @@ resource "snowflake_role" "prod_role" {
   name = "PROD_ROLE"
 }
 
-resource "snowflake_role_grants" "prod_grant_user" {
-  role_name = snowflake_role.prod_role.name
-  users     = [snowflake_user.prod_user.name]
-}
