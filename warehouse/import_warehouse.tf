60f59b319085e84ddab9749fcf6d9afbb8471342
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-21 16:46:08 +0530

    Update import_warehouse.tf

--- warehouse/import_warehouse.tf
+++ warehouse/import_warehouse.tf
@@ -1,3 +1,12 @@
-resource "snowflake_warehouse" "prod_wh" {
-  name = "PROD_WH"
+data "snowflake_warehouse" "existing_warehouse" {
+  name = "COMPUTE_WH"
+}
+
+output "existing_warehouse_info" {
+  value = {
+    name           = data.snowflake_warehouse.existing_warehouse.name
+    size           = data.snowflake_warehouse.existing_warehouse.size
+    auto_suspend   = data.snowflake_warehouse.existing_warehouse.auto_suspend
+    auto_resume    = data.snowflake_warehouse.existing_warehouse.auto_resume
+  }
 }
