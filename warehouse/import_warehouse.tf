cf8723715bead6f7e9f81e13742434ffbbdec27c
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-22 11:27:52 +0530

    Update import_warehouse.tf

--- warehouse/import_warehouse.tf
+++ warehouse/import_warehouse.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.73.0"
+    }
+  }
+}
+
 # Create NEW warehouses
 resource "snowflake_warehouse" "new" {
   for_each = var.new_warehouses
