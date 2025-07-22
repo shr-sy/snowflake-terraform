fe1a9b6dc53a6323be66cb9858a8003b36699503
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-22 11:47:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
+      source  = "Snowflakedb/snowflake"
       version = "~> 0.73.0"
     }
   }
 }
-
+provider "snowflake" {}
 module "warehouse" {
   source = "./warehouse"
   new_warehouses = var.new_warehouses
