e65f9772c6cdc2d6f0fbd934ca48f212d5bb5c61
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-22 11:07:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = ">= 0.27.0"
+    }
+  }
+
+  cloud {
+    organization = "Exercises"
+
+    workspaces {
+      name = "snowflake-terraform"
+    }
+  }
+}
+
+provider "snowflake" {}
+
 module "users" {
   source = "./users"
 }
