933a8ba3b9a8a668e05ccdbf34cb03f0911abe16
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-25 10:54:07 +0530

    Delete policies/phone_mask.tf

--- policies/phone_mask.tf
+++
@@ -1,8 +0,0 @@
-resource "snowflake_masking_policy" "phone_mask" {
-  name              = "PHONE_MASK"
-  database          = "YOUR_DB"
-  schema            = "YOUR_SCHEMA"
-  input_data_type   = "VARCHAR"
-  return_data_type  = "VARCHAR"
-  expression        = "CASE WHEN CURRENT_ROLE() IN ('SYSADMIN') THEN val ELSE '(***) ***-****' END"
-}
