78e86b87cb6c56380d64ca87bae3f33393c25edf
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-25 11:50:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "SNOWFLAKE_ACCOUNT" {}
-variable "SNOWFLAKE_USERNAME" {}
-variable "SNOWFLAKE_PASSWORD" {}
-variable "SNOWFLAKE_ROLE" {}
-
 # Warehouses
 variable "new_warehouses" {
   description = "Map of warehouse names and comments"
