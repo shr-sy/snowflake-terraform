5081d126636b4827cb2527a900ddc5722f48c9bd
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-25 10:53:54 +0530

    Delete policies/email_mask.tf

--- policies/email_mask.tf
+++
@@ -1,8 +0,0 @@
-resource "snowflake_masking_policy" "email_mask" {
-  name              = "EMAIL_MASK"
-  database          = "YOUR_DB"
-  schema            = "YOUR_SCHEMA"
-  input_data_type   = "VARCHAR"
-  return_data_type  = "VARCHAR"
-  expression        = "CASE WHEN CURRENT_ROLE() IN ('SYSADMIN') THEN val ELSE '***@***.com' END"
-}
